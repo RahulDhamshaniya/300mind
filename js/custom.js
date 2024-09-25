f1b403228ebaac9ced7a2a2e0f140c0b9b5e3467
Rahul <rajdhamshaniya@gmail.com>
2024-09-25 09:31:08 +0530

    added new file

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,24 @@
-//tabs
+$(window).scroll(function () {
+     if ($(this).scrollTop() > 50) {
+         $('.header').addClass('fixed');
+     } else {
+         $('.header').removeClass('fixed');
+     }
+ });
+
 
 $(document).ready(function(){ 
+    /*menu*/ 
+    
+    $(".menu-icon #check").click(function () {
+        if ($(".header,html,body").hasClass("is-active")) {
+            $(".header,html,body").removeClass("is-active");
+        } else {
+            $(".header, html,body").addClass("is-active");
+        }
+    });
+
+    /*tabs*/
      $(".link").click(function(){  
        $(".tab").removeClass('tab-active');
        $(".tab[data-id='"+$(this).attr('data-id')+"']").addClass("tab-active");
